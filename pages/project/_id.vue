a42afdd54e7bfbad905fe96d221ee0773b886043
javag97 <javier.garcia549@gmail.com>
2019-11-18 00:58:38 -0800

    dynamic content brought in

--- pages/project/_id.vue
+++ pages/project/_id.vue
@@ -1,15 +1,14 @@
 <template>
    <section>  
-      <h1>{{post.header}}</h1>
-      <p>{{post.content}}</p>
+      <h1>{{header}}</h1>
+      <p>{{content}}</p>
    </section>
 </template>
 <script>
-/*
 import Prismic from "prismic-javascript";
 import PrismicDom from "prismic-dom" //importing the Dom
-import PrismicConfig from "./../prismic.config.js";
-*/
+import PrismicConfig from "../../prismic.config.js";
+
 
 export default {
   data(){
@@ -26,8 +25,9 @@ export default {
      post(){
         return this.posts.find(post => post.id === this.id);
      } 
-  }
-/*
+  },
+  
+
   async asyncData() {
     const api = await Prismic.getApi(PrismicConfig.apiEndpoint);
     let works = {};
@@ -35,16 +35,17 @@ export default {
       Prismic.Predicates.at("document.type", "works"),
       { lang: "en-us" } //This is a Prismic query option
     );
-    console.log("Test 2");
+    console.dir(results);
     works = results.results[0];
+    console.dir(works)
     const header = PrismicDom.RichText.asText(works.data.post_title);
-    const content = PrismicDom.RichText.asText(works.data.post_content);
+    const content = PrismicDom.RichText.asText(works.data.content);
     return { 
         works: works,
         header: header,
         content: content
     };
   } 
-  */
+  
 };
 </script>
